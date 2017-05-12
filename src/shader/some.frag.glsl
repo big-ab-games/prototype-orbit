04b56f6cc03531bed9e830248262b640429bc038
Alex Butler <alexheretic@gmail.com>
2017-05-12 18:39:37 +0100

    Tweak fragment shader

--- src/shader/some.frag.glsl
+++ src/shader/some.frag.glsl
@@ -18,14 +18,12 @@ void main() {
     vec2 coords = f_tex_coords;
 
     float waterline = mix(0.63, 0.67, norm_sin(sin(u_ticks / 1000.)));
+    // wiggle waterline in y-axis
     waterline = waterline + sin(coords.x * 50. + u_ticks / 80.0) / 400.0;
     bool water = coords.y > waterline;
 
     if (water) {
-        // still
-        // coords = vec2(coords.x, waterline * 2.0 - coords.y);
-
-        // sin waves
+        // sin waves wiggle water in x-axis
         coords = vec2(coords.x + sin((coords.y - waterline) * 30.0 + u_ticks / 300.0) / 50.0,
                       waterline * 2.0 - coords.y);
     }
@@ -34,9 +32,7 @@ void main() {
     vec4 sad = texture(t_sad, coords);
     some_target = mix(sad, happy, min(norm_sin(sin(u_ticks / 1000.0)) * 3.0, 1.0));
     if (water) // color the water a bit
-        some_target = some_target * vec4(0.6, 0.7, 1.0, 1.0);
-
-    // some_target = some_target * vec4(f_color, 1.0);
+        some_target = some_target * vec4(0.6, 0.90, 1.0, 1.0);
 
     // make the water a little transparent
     happy = texture(t_happy, f_tex_coords);
